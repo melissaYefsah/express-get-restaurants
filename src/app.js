c492891e49188ea187476c718044c6ffe6f75142
Melissa Yefsah <melissa.yefsah@melissa.yefsah-MacBook-Pro>
2024-11-08 12:46:57 -0500

    added get endpoint for /restaurants/:id

--- src/app.js
+++ src/app.js
@@ -4,7 +4,16 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
-
+app.get('/restaurants',async (req,res)=>{
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+})
+app.get('/restaurants/:id',async (req,res)=>{
+    const parameter = req.params.id;
+    const restaurant = await Restaurant.findByPk(parameter);
+    res.json(restaurant);
+    
+})
 
 
 
